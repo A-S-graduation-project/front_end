0341786645d83b75a48f750bc05c1dc1c9fc777a
SangJun Han <grw05177@naver.com>
2023-04-02 00:29:35 +0900

    Test psql and Creat comment

--- allergyProject/allergyProject/settings.py
+++ allergyProject/allergyProject/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default' : {
+    #     'ENGINE' : 'django.db.backends.postgresql',
+    #     'NAME' : 'foods',
+    #     'USER' : 'postgres',
+    #     'PASSWORD' : '2017018',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '',
+    # }
 }
 
 
