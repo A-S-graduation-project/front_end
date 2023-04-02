3c8aa54c52e0964db7c9c05c2c49c255b175f8b5
SangJun <grw05177@naver.com>
2023-04-02 19:15:52 +0900

    Add allergy buttons and hashtag below the search bar

--- UI/app.js
+++ UI/app.js
@@ -29,3 +29,59 @@ function changeSlide(n) {
   
   showSlides();
 }
+
+// Get the search input and selected-allergens element
+const searchInput = document.getElementById('search-input');
+const selectedAllergens = document.getElementById('selected-allergens');
+const allergenBtnsContainer = document.getElementById('allergen-btns-container');
+
+// Add event listener to allergen buttons
+const allergenBtns = document.querySelectorAll('.allergen-btn');
+allergenBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // Get the selected allergen and add to selected-allergens element
+    const allergen = btn.getAttribute('data-allergen');
+    
+    // Check if the hashtag already exists and add it only if it does not exist
+    const hashtagExists = [...selectedAllergens.querySelectorAll('.hashtag')].some(hashtag => hashtag.textContent === `#${allergen}`);
+    if (!hashtagExists) {
+      const hashtag = document.createElement('span');
+      hashtag.classList.add('hashtag');
+      hashtag.textContent = `#${allergen}`;
+      selectedAllergens.appendChild(hashtag);
+    }
+
+    // Clear the search input and add the selected allergen to the query string
+    searchInput.value = '';
+    const params = new URLSearchParams(window.location.search);
+    params.set('allergen', allergen);
+    window.location.search = params.toString();
+  });
+});
+
+// Add event listener to hashtags for deselecting allergens
+selectedAllergens.addEventListener('click', e => {
+  if (e.target.classList.contains('hashtag')) {
+    // Remove the hashtag and update the query string
+    const allergen = e.target.textContent.slice(1);
+    e.target.remove();
+    const params = new URLSearchParams(window.location.search);
+    params.delete('allergen');
+    window.location.search = params.toString();
+  }
+});
+
+// Fill the search input with the selected allergen on page load
+const params = new URLSearchParams(window.location.search);
+const allergen = params.get('allergen');
+if (allergen) {
+  const hashtag = document.createElement('span');
+  hashtag.classList.add('hashtag');
+  hashtag.textContent = `#${allergen}`;
+  selectedAllergens.appendChild(hashtag);
+  searchInput.value = allergen;
+}
+
+// Append the allergen buttons container to the search form
+const searchForm = document.querySelector('.search form');
+searchForm.appendChild(allergenBtnsContainer);
